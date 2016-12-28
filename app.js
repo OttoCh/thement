7e3c9dfd5204b738ea69097b9103f34667283481
Wildan S. Nahar <wildansnahar@gmail.com>
2016-12-28 20:37:08 +0700

    register routes

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express         = require('express'),
     session         = require('express-session'),
     MongoStore      = require('connect-mongo')(session),
-    bodyParser      = require('body-parser'),
     methodOverride  = require('method-override'),
     http            = require('http'),
     port         		= process.env.PORT || 3500;
     app             = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('X-HTTP-Method-Override'));
 
-app.get('/', function(req, res){
-  res.send('thement')
-})
+// load routes
+var student         = require('./routes/student.route')
+var index           = require('./routes/index.route')
+
+app.use('/', index)
+app.use('/api/v1/student', student)
 
 app.listen(port)
 console.log('Thement running at ', port)
