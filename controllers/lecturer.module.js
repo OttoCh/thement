0e6a3602f72bfbebc8153531e567655617e2f97f
Wildan S. Nahar <wildansnahar@gmail.com>
2017-01-06 20:35:19 +0700

    login page styling

--- controllers/lecturer.module.js
+++ controllers/lecturer.module.js
@@ -2,5 +2,9 @@ var Lect        = require('../models/lecturer')
 const baseurl   = 'http://localhost:3500/lecturer'
 
 exports.getIndex = function(req, res){
+  res.redirect('lecturer/login')
+}
+
+exports.getLoginPage = function(req, res){
   res.render('lecturer/login', {title:"Lecturer login page"})
 }
--- routes/lecturer.route.js
+++ routes/lecturer.route.js
@@ -6,5 +6,6 @@ const baseurl   = 'http://localhost:3500/lecturer'
 var lecturer    = require('../controllers/lecturer.module')
 
 router.get('/', lecturer.getIndex)
+router.get('/login', lecturer.getLoginPage)
 
 module.exports = router
