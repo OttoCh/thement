cd62259b8b87a814025566f7afe80b94d48dd649
Wildan S. Nahar <wildansnahar@gmail.com>
2017-01-06 08:05:49 +0700

    add lecturer's sidebar

--- controllers/student.choose.js
+++ controllers/student.choose.js
@@ -1,11 +1,11 @@
 // load lecturers
-var lect = require('../models/lecturer.model')
-
+var lect      = require('../models/lecturer.model')
+const baseurl = 'http://localhost:3500/student'
 
 exports.getLecturers = function(req, res){
   let nim = req.session.student
   lect.all(function(err, lecturers){
-    res.render('student/lecturers', {title:"All lecturers", nim:nim, lecturers:lecturers})
+    res.render('student/lecturers', {title:"All lecturers", nim:nim, lecturers:lecturers, baseurl:baseurl})
   })
 }
 
@@ -13,7 +13,7 @@ exports.getDetailLecturer = function(req, res){
   let nim = req.session.student
   console.log(req.url)
   lect.get(req.params.username, function(err, lecturer){
-    res.render('student/lecturer-detail', {title:"Lecturer detail", nim:nim, lecturer:lecturer})
+    res.render('student/lecturer-detail', {title:"Lecturer detail", nim:nim, lecturer:lecturer, baseurl:baseurl})
   })
 }
 
