66eb3ad3f4234bac436ca17ca727f14585066803
Wildan S. Nahar <wildansnahar@gmail.com>
2017-01-11 16:11:55 +0700

    redefine conversation schema

---
+++ models/conversation.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var convSchema = new Schema ({
+  from: String,
+  to: String,
+  messages: [{
+    date: Date,
+    message: String,
+    has_seen: Boolean
+  }]
+})
+
+module.exports = mongoose.model('Conversation', convSchema)
