2ad0dd5987f9ae585fc3cded2a7f82ebe194a138
Wildan S. Nahar <wildansnahar@gmail.com>
2017-02-17 16:42:23 +0700

    showing notifs

--- controllers/student.notif.js
+++ controllers/student.notif.js
@@ -6,13 +6,48 @@ const baseurl = 'http://localhost:3500/student'
 
 var hiding     = 'hide'
 var chooseCode = ''
+friendlyDate = function(tgl){
+    month = tgl.getMonth(),
+    month = month+1,
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','Desember'],
+    month  = months[tgl.getMonth()]
+    time = tgl.getHours()+':'+tgl.getMinutes()+' WIB'
+    tgl = month + ' '+ tgl.getDate()+', '+ tgl.getFullYear()+' at '+time
+    return tgl
+}
 
 exports.getNotifs = function(req, res){
-  res.send('all notifs')
+  var nim        = req.session.student
+  student.findOne({nim:nim}, function(err, std){
+    let objNotifs = [],
+        data      = std.notifs
+    for(var i=0; i<data.length; i++){
+      objNotifs.push({
+        index:i,
+        notif:data[i].notif,
+        date:friendlyDate(data[i].date)
+      })
+    }
+    var notifs = objNotifs.reverse()
+    console.log(notifs)
+    res.render('student/notif/notifs', {title:"All Notifications", notifs:notifs})
+  })
 }
 
 exports.getSingleNotif = function(req, res){
-  res.send('single notif')
+  var nim        = req.session.student
+  student.findOne({nim:nim}, function(err, std){
+    let objNotifs = [],
+        data      = std.notifs
+    for(var i=0; i<data.length; i++){
+      objNotifs.push({
+        index:i,
+        notif:data[i].notif,
+        date:friendlyDate(data[i].date)
+      })
+    }
+    res.render('student/notif/notif-single', {title:"Single notif"})
+  })
 }
 
 exports.removeAllNotifs = function(req, res){
