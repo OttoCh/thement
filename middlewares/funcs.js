de061971cf0753a88ee79ad7d2d152478673d8b8
Wildan S. Nahar <wildansnahar@gmail.com>
2017-02-19 20:46:51 +0700

    date format bug fixings

--- middlewares/funcs.js
+++ middlewares/funcs.js
@@ -1,33 +1,37 @@
 module.exports = {
   friendlyDate: function(tgl){
       let now = new Date()
-
-      // check if now is more than 7 days
       let nowDate = now.getDate()
       let tglDate = tgl.getDate()
       let diff    = nowDate - tglDate
-      if(diff > 7){
-        month = tgl.getMonth(),
-        month = month+1,
-        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','Desember'],
-        month  = months[tgl.getMonth()]
-        time = tgl.getHours()+':'+tgl.getMinutes()+' WIB'
-        tgl = month + ' '+ tgl.getDate()+', '+ tgl.getFullYear()+' at '+time
-      } else if(diff < 1) {
-        // check diffhours
-        var tglMin = tgl.getMinutes()
-        if(tglMin < 10){
-          tglMin = '0'+tgl.getMinutes()
-        } else {
-          tglMin = tgl.getMinutes()
-        }
-        time = tgl.getHours()+':'+tglMin+' WIB'
-        tgl = 'Today at : '+ time
-      } else if(diff > 1 && diff < 7){
-        console.log('more than a day and less than 7 days')
-        tgl = diff + ' days ago'
+      month = tgl.getMonth(),
+      month = month+1,
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','Desember'],
+      month  = months[tgl.getMonth()]
+      var tglMin = tgl.getMinutes()
+      if(tglMin < 10){
+        tglMin = '0'+tgl.getMinutes()
       } else {
-        tgl = 'Not detected'
+        tglMin = tgl.getMinutes()
+      }
+
+      time = tgl.getHours()+':'+tglMin+' WIB'
+      tgl = month + ' '+ tgl.getDate()+', '+ tgl.getFullYear()+' at '+time
+
+      if(diff > 7){
+          tgl = tgl
+      }
+      else if((diff > 1) && (diff < 7)){
+        tgl = diff + ' days ago'
+      }
+      else if(diff < 1) {
+        tgl = 'Today at : '+ time
+      }
+      else if(diff == 1){
+        tgl = 'Yesterday at '+ time
+      }
+      else {
+        tgl = tgl
       }
       return tgl
   }
