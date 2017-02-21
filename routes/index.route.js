7f021bacf0085545a4457ee8f466852e6a45d4d0
Wildan S. Nahar <wildansnahar@gmail.com>
2017-02-21 14:28:23 +0700

    add homepage successfull

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,11 +2,7 @@ var express   = require('express'),
     router    = express.Router()
 
 router.get('/', function(req, res){
-  res.json({
-    "Status":"OK",
-    "Message":"Homepage",
-    "Session":req.session.fullname
-  })
+  res.render('static/home', {title:"Homepage"})
 })
 
 module.exports = router
