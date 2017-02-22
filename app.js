90370942559edb4590df61e9a8c93d2cb0b7f4cd
Wildan S. Nahar <wildansnahar@gmail.com>
2017-02-22 11:22:28 +0700

    add testing

--- app.js
+++ app.js
@@ -12,41 +12,41 @@ var express         = require('express'),
     path            = require('path'),
     morgan          = require('morgan'),
     compression     = require('compression'),
-    app             = express()
+    app             = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/tugasakhir')
+mongoose.connect('mongodb://127.0.0.1:27017/tugasakhir');
 
-app.all('/api/v1/*', [require('./middlewares/auth')])
+app.all('/api/v1/*', [require('./middlewares/auth')]);
 
 app.use(session({
   secret: 'idf032nasd',
   store: new redisStore({host: 'localhost', port: 6379, client:client, ttl: 260}),
   saveUninitialized: false,
   resave: false
-}))
+}));
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(methodOverride('X-HTTP-Method-Override'))
-app.use(morgan('dev'))
-app.use(compression())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(morgan('dev'));
+app.use(compression());
 app.use('/static', express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade')
+app.set('view engine', 'jade');
 
-var student_apis    = require('./routes/api.student.route')
-var student         = require('./routes/student.route')
-var lecturer        = require('./routes/lecturer.route')
+var student_apis    = require('./routes/api.student.route');
+var student         = require('./routes/student.route');
+var lecturer        = require('./routes/lecturer.route');
 
-var admin           = require('./routes/admin.route')
-var index           = require('./routes/index.route')
+var admin           = require('./routes/admin.route');
+var index           = require('./routes/index.route');
 
-app.use('/', index)
-app.use('/student', student)
-app.use('/lecturer', lecturer)
-app.use('/api/v1/student', student_apis)
-app.use('/api/v1/admin', admin)
+app.use('/', index);
+app.use('/student', student);
+app.use('/lecturer', lecturer);
+app.use('/api/v1/student', student_apis);
+app.use('/api/v1/admin', admin);
 
-app.listen(3500)
-console.log('Thement running at 3500')
+app.listen(3500);
+console.log('Thement running at 3500');
