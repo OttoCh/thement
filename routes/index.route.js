7d5979925e6ae1b72bbcb4351f23cf0da086979f
wldn <wildansnahar@gmail.com>
2017-02-26 02:08:18 +0700

    remove localIP detection

--- routes/index.route.js
+++ routes/index.route.js
@@ -5,12 +5,6 @@ var express   = require('express'),
     router    = express.Router()
 
 router.get('/', function(req, res){
-  // get local ip address
-  let localIP = funcs.getLocalIP()
-  console.log('INI LOKAL IP : ', localIP)
-  let msg = 'test'
-  let output = funcs.jsonOutput(msg)
-  console.log(output)
   res.render('static/home', {title:"Homepage"})
 })
 
